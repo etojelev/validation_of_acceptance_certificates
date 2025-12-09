d5f992fe5bef293c8dd431d30ef573d39eff400e
etojelev <lion.patskevich@yandex.ru>
2025-12-09 17:24:56 +0300

    Add: add base fastapi application configs

--- src/main.py
+++ src/main.py
@@ -1,6 +1,24 @@
-def main() -> None:
-    print("Hello from validation-of-acceptance-certificates!")
+from typing import Any
+
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 
-if __name__ == "__main__":
-    main()
+def add_middleware(app: FastAPI, *args: Any, **kwargs: Any) -> None:
+    app.add_middleware(*args, **kwargs)
+
+
+def start_application() -> FastAPI:
+    application = FastAPI(title="Validation Acceptance Certificates")
+    add_middleware(
+        application,
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+    return application
+
+
+app = start_application()
