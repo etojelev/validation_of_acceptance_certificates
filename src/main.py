320ead38d916ffde8543e9535d48a473b117e170
etojelev <lion.patskevich@yandex.ru>
2025-12-10 10:26:24 +0300

    Fix: add lifespan into application

--- src/main.py
+++ src/main.py
@@ -1,7 +1,20 @@
+from collections.abc import AsyncGenerator
+from contextlib import asynccontextmanager
 from typing import Any
 
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
+
+from src.dependencies.database import check_pool_created, check_pool_stopped
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncGenerator:
+    # выполнение проверки создания пулов соединений к бд ДО запуска приложения
+    await check_pool_created()
+    yield
+    # выполнение проверки отключений пулов соединений к бд ПОСЛЕ запуска приложения
+    await check_pool_stopped()
 
 
 def add_middleware(app: FastAPI, *args: Any, **kwargs: Any) -> None:
@@ -9,7 +22,7 @@ def add_middleware(app: FastAPI, *args: Any, **kwargs: Any) -> None:
 
 
 def start_application() -> FastAPI:
-    application = FastAPI(title="Validation Acceptance Certificates")
+    application = FastAPI(title="Validation Acceptance Certificates", lifespan=lifespan)
     add_middleware(
         application,
         CORSMiddleware,
