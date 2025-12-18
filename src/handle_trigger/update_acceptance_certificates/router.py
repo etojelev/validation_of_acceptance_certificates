c862ec3f3c21903bb62419f0ddda327ce2991262
etojelev <lion.patskevich@yandex.ru>
2025-12-18 17:32:08 +0300

    Add: add handler trigger endpoint for updatind acceptance certificates

---
+++ src/handle_trigger/update_acceptance_certificates/router.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter, Depends
+
+from src.celery.tasks.document_service import DocumentsService
+from src.dependencies.handle_trigger.update_acceptance_certificates import (
+    get_documents_validation_repository,
+)
+
+update_certificates = APIRouter(
+    prefix="/system/handle_trigger", tags=["/system/handle_trigger"]
+)
+
+
+@update_certificates.post("/update_acceptance_certificates")
+async def update_acceptance_certificates(
+    documents_service: DocumentsService = Depends(get_documents_validation_repository),
+) -> dict[str, str | int]:
+    await documents_service._sync_update_acceptance_certificates()
+    return {"status": 201, "message": "database updated"}
