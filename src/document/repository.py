c824fc7130a03ab483e6adf50a2f1749a141933d
etojelev <lion.patskevich@yandex.ru>
2025-12-22 17:55:27 +0300

    Add: add 'get_validated_order'

--- src/document/repository.py
+++ src/document/repository.py
@@ -1,3 +1,4 @@
+from datetime import date
 from logging import getLogger
 from typing import Any
 
@@ -7,6 +8,7 @@ from asyncpg import (
     InterfaceError,
     PostgresError,
 )
+from asyncpg.protocol import Record
 
 from src.dependencies.database import DatabasePoolManager
 from src.utils.decorators import error_handler_http
@@ -39,3 +41,48 @@ class DocumentsRepository:
         """
 
         await self.database.executemany(query, certificates)
+
+    @error_handler_http(
+        status_code=500,
+        message="Database occure error",
+        exceptions=(
+            PostgresError,
+            InterfaceError,
+            ConnectionFailureError,
+            ConnectionDoesNotExistError,
+        ),
+    )
+    async def get_validated_orders(
+        self,
+        begin_date: date,
+        end_date: date,
+        order_id: int,
+        supply_id: str,
+        account: str,
+        page_size: int,
+        offset: int,
+    ) -> Record:
+        query = """
+        SELECT
+            osl.order_id,
+            osl.supply_id,
+            afa.sticker,
+            osl.status,
+            afa.document,
+            afa.account,
+            afa.date
+        FROM order_status_log osl
+        LEFT JOIN acceptance_fbs_acts_new afa
+        ON osl.order_id::text = afa.order_number
+        WHERE
+            ($1::date IS NULL OR afa.date >= $1::date) AND
+            ($2::date IS NULL OR afa.date <= $2::date) AND
+            ($3::int8 IS NULL OR osl.order_id = $3::int8) AND
+            ($4::varchar IS NULL OR osl.supply_id = $4::varchar) AND
+            ($5::varchar IS NULL or afa.account = $5::varchar)
+        LIMIT $6
+        OFFSET $7;
+        """
+        return await self.database.fetch(
+            query, begin_date, end_date, order_id, supply_id, account, page_size, offset
+        )
--- src/document/schema.py
+++ src/document/schema.py
@@ -1,9 +1,29 @@
-from datetime import datetime
+from datetime import date, datetime
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class DocumentSchema(BaseModel):
     act_income_name: str
     supply_id: str
     document_created_at: datetime
+
+
+class ValidatedOrder(BaseModel):
+    order_id: int = Field(description="ID сборочного задания")
+    supply_id: str | None = Field(
+        description="ID поставки (None, если СЗ ещё не добавлена в поставку)"
+    )
+    sticker: int | None = Field(
+        description="Стикер (None, если акта для данного СЗ нет)"
+    )
+    inner_order_status: str = Field(description="Внутренний статус")
+    document: str | None = Field(
+        description="Наименования акта приёма передачи (None, если акта ещё нет)"
+    )
+    account: str | None = Field(
+        description="Наименование аккаунта (None, если акта ещё нет)"
+    )
+    document_date: date | None = Field(
+        description="Дата создания акта приёма передачи (None, если СЗ ещё не добавлена в поставку)"
+    )
